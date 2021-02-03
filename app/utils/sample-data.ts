52e2dec6d2a7a52af5a719879d6a077fe0ef1d02
kawakamygg <j.kawakami.dev@gmail.com>
2021-02-03 18:39:41 +0900

    ✨ 記事のダミーデータを追加

--- app/utils/sample-data.ts
+++ app/utils/sample-data.ts
@@ -1,4 +1,4 @@
-import { User } from '../interfaces'
+import { User, Article } from '../interfaces'
 
 /** Dummy user data. */
 export const sampleUserData: User[] = [
@@ -6,4 +6,32 @@ export const sampleUserData: User[] = [
   { id: 102, name: 'Bob' },
   { id: 103, name: 'Caroline' },
   { id: 104, name: 'Dave' }
+]
+
+/** Dummy article data. */
+export const sampleArticleData: Article[] = [
+  {
+    id: 'AnxKyXkjHWhJexY',
+    title: 'article_01',
+    content: 'this is article_01',
+    auther: { id: 101, name: 'Alice' },
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    id: 'BnxKyXkjHWhJexY',
+    title: 'article_02',
+    content: 'this is article_02',
+    auther: { id: 102, name: 'Bob' },
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    id: 'CnxKyXkjHWhJexY',
+    title: 'article_03',
+    content: 'this is article_03',
+    auther: { id: 103, name: 'Caroline' },
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
 ]
