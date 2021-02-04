d697d2bc566cb27fcf46059627b4746fa9ba36f0
kawakamygg <j.kawakami.dev@gmail.com>
2021-02-04 17:44:55 +0900

    ✨ github-markdown-css 読み込み

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import 'github-markdown-css'
 import type { AppProps } from 'next/app'
+import './_app.scss'
 import 'normalize.css'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
