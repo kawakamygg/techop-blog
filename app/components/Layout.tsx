c14c259768ebd354a90cce7bfcdbfc81aefc9a0d
kawakamygg <j.kawakami.dev@gmail.com>
2021-02-04 13:57:23 +0900

    ✨ フッターを最下部に固定

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react'
 import Head from 'next/head'
-
+import style from './Layout.module.scss'
 import Header from './Header'
 import Footer from './Footer'
 
@@ -10,14 +10,14 @@ type Props = {
 }
 
 const Layout: React.FC<Props> = ({ children, title = '' }: Props) => (
-  <div>
+  <div className={style.s}>
     <Head>
       <title>{`${title ? `${title} | ` : ''}Techop`}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <Header />
-    {children}
+    <div className={style.s_contents}>{children}</div>
     <Footer />
   </div>
 )
